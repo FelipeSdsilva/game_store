be674e92861c0d798961049fde152b5a68b245b8
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-29 18:26:50 -0300

    Incluindo o construtores em UsuarioLogin e sobrecargas

--- src/main/java/com/generation/game_store/dto/UsuarioLogin.java
+++ src/main/java/com/generation/game_store/dto/UsuarioLogin.java
@@ -1,5 +1,8 @@
 package com.generation.game_store.dto;
 
+import com.generation.game_store.entities.Usuario;
+import org.springframework.beans.BeanUtils;
+
 public class UsuarioLogin {
 
     private Long id;
@@ -8,6 +11,23 @@ public class UsuarioLogin {
     private String senha;
     private String foto;
     private String token;
+
+    public UsuarioLogin() {
+    }
+
+    public UsuarioLogin(Long id, String nome, String usuario, String senha, String foto, String token) {
+        this.id = id;
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.foto = foto;
+        this.token = token;
+    }
+
+    public UsuarioLogin(Usuario entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
 
     public Long getId() {
         return id;
