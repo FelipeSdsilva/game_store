67c6ab791bf30db5a1e175130f207d439bb8a6a6
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-29 11:50:18 -0300

    Criando dtos UsuarioDTO e UsuarioLogin

--- src/main/java/com/generation/game_store/dto/UsuarioDTO.java
+++ src/main/java/com/generation/game_store/dto/UsuarioDTO.java
@@ -1,2 +1,68 @@
-package com.generation.game_store.dto;public class UsuarioDTO {
+package com.generation.game_store.dto;
+
+import com.generation.game_store.entities.Usuario;
+import org.springframework.beans.BeanUtils;
+
+public class UsuarioDTO {
+
+    private Long id;
+    private String nome;
+    private String usuario;
+    private String senha;
+    private String foto;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(Long id, String nome, String usuario, String senha, String foto) {
+        this.id = id;
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.foto = foto;
+    }
+
+    public UsuarioDTO(Usuario entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
