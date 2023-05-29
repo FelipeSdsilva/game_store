f3254a19925e6503583ff7b2116e3f364219a1ad
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-29 11:56:52 -0300

    Implementando métodos get, put e post em UsuarioController

--- src/main/java/com/generation/game_store/controllers/UsuarioController.java
+++ src/main/java/com/generation/game_store/controllers/UsuarioController.java
@@ -1,2 +1,58 @@
-package com.generation.game_store.controllers;public class UsuarioController {
+package com.generation.game_store.controllers;
+
+import com.generation.game_store.dto.UsuarioDTO;
+import com.generation.game_store.dto.UsuarioLogin;
+import com.generation.game_store.services.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> getAllUsers() {
+        return ResponseEntity.ok().body(usuarioService.listarTodosOsUsuarios());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDTO> getById(@PathVariable Long id) {
+        return usuarioService.findById(id)
+                .map(resp -> ResponseEntity.ok(resp))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/logar")
+    public ResponseEntity<UsuarioLogin> autenticarUsuario(@RequestBody Optional<UsuarioLogin> usuarioLogin) {
+        return usuarioService.autenticarUsuario(usuarioLogin)
+                .map(resp -> ResponseEntity.status(HttpStatus.OK).body(resp))
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<UsuarioDTO> postUsuario(@RequestBody @Valid UsuarioDTO usuario) {
+
+        return usuarioService.cadastrarUsuario(usuario)
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+
+    }
+
+    @PutMapping("/atualizar")
+    public ResponseEntity<UsuarioDTO> putUsuario(@Valid @RequestBody UsuarioDTO usuario) {
+        return usuarioService.atualizarUsuario(usuario)
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+    }
+
 }
